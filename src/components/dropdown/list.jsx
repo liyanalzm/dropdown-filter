3a22040a0e280d00eb7c4c6f906f225e21dbe03d
Liyana <liyana@wego.com>
2020-03-03 11:53:12 +0800

    Add text no results found

--- src/components/dropdown/list.jsx
+++ src/components/dropdown/list.jsx
@@ -5,25 +5,27 @@ import { useContext } from "react";
 import { DropdownAPI } from ".";
 
 const List = ({ list, isOpen, handleOptionSelected, inset }) => {
-  const { value: selectedOption } = useContext(
-    DropdownAPI
-  );
+  const { value: selectedOption } = useContext(DropdownAPI);
 
   return isOpen ? (
     <div className={`list ${!inset ? "popout" : ""}`} id="list">
-      {list.map(({ value, label }) => (
-        <div
-          className={`list__item ${
-            selectedOption && selectedOption.value === value
-              ? "list__item-active"
-              : ""
-          }`}
-          onClick={() => handleOptionSelected({ value, label })}
-          key={value}
-        >
-          {label}
-        </div>
-      ))}
+      {list.length > 0 ? (
+        list.map(({ value, label }) => (
+          <div
+            className={`list__item ${
+              selectedOption && selectedOption.value === value
+                ? "list__item-active"
+                : ""
+            }`}
+            onClick={() => handleOptionSelected({ value, label })}
+            key={value}
+          >
+            {label}
+          </div>
+        ))
+      ) : (
+        <div className="list__item">No results found</div>
+      )}
     </div>
   ) : null;
 };
